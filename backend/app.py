f0981e7f7042326aab026cd2dc364f5011e2c784
Jesús Escribano Serena <jesusescribano2002@gmail.com>
2023-08-30 09:48:10 +0200

    Modify gitignore to delete VSCode

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ from flask_marshmallow import Marshmallow
 from secret import rutaSQL
 
 app = Flask(__name__)
-CORS(app, resources={r'/*': {'origins': '*'}})
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = rutaSQL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -32,18 +32,55 @@ class Cliente(db.Model):
         self.poblacion = poblacion
         self.telefono = telefono
 
+class Factura(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    idCliente = db.Column(db.Integer)
+    fecha = db.Column(db.DateTime, default = datetime.datetime.now)
+    productos = db.relationship('Producto', backref = 'factura', lazy = True)
+    importeTotal = db.Column(db.Float)
+
+    def __init__(self, idCliente, productos, importe):
+        self.idCliente = idCliente
+        self.productos = productos
+        self.importe = importe
+
+class Producto(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    idFactura = db.Column(db.Integer, db.ForeignKey('factura.id'))
+    nombre = db.Column(db.String(50))
+    precio = db.Column(db.Float)
+    cantidad = db.Column(db.Integer)
+    importe = db.Column(db.Float)
+
+    def __init__(self, idFactura, nombre, precio, cantidad, importe):
+        self.idFactura = idFactura
+        self.nombre = nombre
+        self.precio = precio
+        self.cantidad = cantidad
+        self.importe = importe
+
 class ClienteSchema(ma.Schema):
     class Meta:
         fields = ('id', 'razonSocial', 'nombre', 'nif', 'direccion', 'poblacion', 'telefono')
 
+class ClienteSimplificadoSchema(ma.Schema):
+    class Meta:
+        fields = ('razonSocial', 'id')
+
 cliente_schema = ClienteSchema()
 clientes_schema = ClienteSchema(many = True)
+clientes_simplificado_schema = ClienteSimplificadoSchema(many = True)
 
 @app.route('/getClients', methods = ['GET'])
-@cross_origin()
 def get_clients():
     all_clients = Cliente.query.all()
     result = clientes_schema.dump(all_clients)
+    return jsonify(result)
+
+@app.route('/getClientsSimplificado', methods = ['GET'])
+def get_clients_simplificado():
+    all_clients = Cliente.query.all()
+    result = clientes_simplificado_schema.dump(all_clients)
     return jsonify(result)
 
 @app.route('/getClient/<id>/', methods = ['GET'])
